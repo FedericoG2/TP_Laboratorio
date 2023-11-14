cbf94f57b6dd45a733cd41569fc464d2145d69b9
FedericoG2 <fg0170700@gmail.com>
2023-11-14 18:29:33 -0300

    Agregar archivos de proyecto.

---
+++ TP_Laboratorio/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace TP_Laboratorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.optImportado = new System.Windows.Forms.RadioButton();
+            this.optNacional = new System.Windows.Forms.RadioButton();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.txtNumRepuesto = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.optImportadoConsulta = new System.Windows.Forms.RadioButton();
+            this.optNacionalConsulta = new System.Windows.Forms.RadioButton();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbMarcaConsulta = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.Numero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Nu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnLimpiar);
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtDescripcion);
+            this.groupBox1.Controls.Add(this.optImportado);
+            this.groupBox1.Controls.Add(this.optNacional);
+            this.groupBox1.Controls.Add(this.cmbMarca);
+            this.groupBox1.Controls.Add(this.txtNumRepuesto);
+            this.groupBox1.Location = new System.Drawing.Point(25, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(747, 211);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de Repuestos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 172);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Precio";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Descripcion";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Origen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Marca";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Numero Repuesto";
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(539, 81);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(86, 36);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(539, 19);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(86, 36);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "&Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(333, 165);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 5;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(333, 139);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(130, 20);
+            this.txtDescripcion.TabIndex = 4;
+            // 
+            // optImportado
+            // 
+            this.optImportado.AutoSize = true;
+            this.optImportado.Location = new System.Drawing.Point(333, 104);
+            this.optImportado.Name = "optImportado";
+            this.optImportado.Size = new System.Drawing.Size(72, 17);
+            this.optImportado.TabIndex = 3;
+            this.optImportado.TabStop = true;
+            this.optImportado.Text = "Importado";
+            this.optImportado.UseVisualStyleBackColor = true;
+            // 
+            // optNacional
+            // 
+            this.optNacional.AutoSize = true;
+            this.optNacional.Location = new System.Drawing.Point(333, 81);
+            this.optNacional.Name = "optNacional";
+            this.optNacional.Size = new System.Drawing.Size(67, 17);
+            this.optNacional.TabIndex = 2;
+            this.optNacional.TabStop = true;
+            this.optNacional.Text = "Nacional";
+            this.optNacional.UseVisualStyleBackColor = true;
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(333, 45);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(100, 21);
+            this.cmbMarca.TabIndex = 1;
+            // 
+            // txtNumRepuesto
+            // 
+            this.txtNumRepuesto.Location = new System.Drawing.Point(333, 19);
+            this.txtNumRepuesto.Name = "txtNumRepuesto";
+            this.txtNumRepuesto.Size = new System.Drawing.Size(100, 20);
+            this.txtNumRepuesto.TabIndex = 0;
+            this.txtNumRepuesto.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGrid);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.optImportadoConsulta);
+            this.groupBox2.Controls.Add(this.optNacionalConsulta);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.cmbMarcaConsulta);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.btnConsultar);
+            this.groupBox2.Location = new System.Drawing.Point(27, 229);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(747, 209);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Consulta de Repuestos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "&Todos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // optImportadoConsulta
+            // 
+            this.optImportadoConsulta.AutoSize = true;
+            this.optImportadoConsulta.Location = new System.Drawing.Point(92, 109);
+            this.optImportadoConsulta.Name = "optImportadoConsulta";
+            this.optImportadoConsulta.Size = new System.Drawing.Size(72, 17);
+            this.optImportadoConsulta.TabIndex = 13;
+            this.optImportadoConsulta.TabStop = true;
+            this.optImportadoConsulta.Text = "Importado";
+            this.optImportadoConsulta.UseVisualStyleBackColor = true;
+            // 
+            // optNacionalConsulta
+            // 
+            this.optNacionalConsulta.AutoSize = true;
+            this.optNacionalConsulta.Location = new System.Drawing.Point(92, 77);
+            this.optNacionalConsulta.Name = "optNacionalConsulta";
+            this.optNacionalConsulta.Size = new System.Drawing.Size(67, 17);
+            this.optNacionalConsulta.TabIndex = 12;
+            this.optNacionalConsulta.TabStop = true;
+            this.optNacionalConsulta.Text = "Nacional";
+            this.optNacionalConsulta.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Origen";
+            // 
+            // cmbMarcaConsulta
+            // 
+            this.cmbMarcaConsulta.FormattingEnabled = true;
+            this.cmbMarcaConsulta.Location = new System.Drawing.Point(92, 36);
+            this.cmbMarcaConsulta.Name = "cmbMarcaConsulta";
+            this.cmbMarcaConsulta.Size = new System.Drawing.Size(67, 21);
+            this.cmbMarcaConsulta.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Marca";
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(32, 151);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(79, 29);
+            this.btnConsultar.TabIndex = 7;
+            this.btnConsultar.Text = "&Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Numero
+            // 
+            this.Numero.HeaderText = "Numero";
+            this.Numero.Name = "Numero";
+            this.Numero.Width = 50;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nu,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGrid.Location = new System.Drawing.Point(290, 19);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.Size = new System.Drawing.Size(451, 167);
+            this.dataGrid.TabIndex = 15;
+            // 
+            // Nu
+            // 
+            this.Nu.HeaderText = "Numero";
+            this.Nu.Name = "Nu";
+            this.Nu.ReadOnly = true;
+            this.Nu.Width = 70;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Marca";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Origen";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Descripcion";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Precio";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtNumRepuesto;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.RadioButton optImportado;
+        private System.Windows.Forms.RadioButton optNacional;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbMarcaConsulta;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton optImportadoConsulta;
+        private System.Windows.Forms.RadioButton optNacionalConsulta;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Numero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ TP_Laboratorio/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_Laboratorio
+{
+    public partial class Form1 : Form
+    {
+       //Valores constantes, marcas y origen
+
+        const string P = "Peugeot";
+        const string F = "Fiat";
+        const string R = "Renault";
+
+        const string N = "Nacional";
+        const string I = "Importado";
+
+        
+
+        //Variables para guardar datos de la interfaz
+
+        int numRepuesto;
+        string marca;
+        string origen;
+        string descripcion;
+        float precio;
+
+        
+        //Variables para guardar los valores de los filtros seleccionados
+
+        string marcaFiltro;
+        string origenFiltro;
+
+
+
+
+        //Estructura
+
+        public struct REPUESTO
+        {
+            public int numero;
+            public string marca;
+            public string origen;
+            public string descripcion;
+            public float precio;
+        }
+
+        //Arreglo
+
+        REPUESTO[] repuestos;
+
+    
+
+        //Posicion en el arreglo
+        public int posicion = 0;
+
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            //Carga combo marca
+            cmbMarca.Items.Add(P);
+            cmbMarca.Items.Add(F);
+            cmbMarca.Items.Add(R);
+
+            //Seleccionado el primero
+            cmbMarca.SelectedIndex = 0;
+
+            //Inicializo el arreglo en 10 elementos
+            repuestos = new REPUESTO[10];
+
+            
+          
+
+           //Carga combo marca pero del sector donde se aplican los filtros
+            cmbMarcaConsulta.Items.Add(P);
+            cmbMarcaConsulta.Items.Add(F);
+            cmbMarcaConsulta.Items.Add(R);
+
+            
+
+           
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Evento Registrar :
+
+            //  1.Almaceno en variables los datos de la interfaz.
+
+            //  2.Guardo los datos en el arreglo de estructuras. 
+
+            //  3.Limpio la interfaz.
+
+
+            //Numero de respuesto
+            numRepuesto = int.Parse(txtNumRepuesto.Text);
+
+            //Marca
+            marca = cmbMarca.SelectedItem.ToString();
+
+            //Origen
+            if (optNacional.Checked) {
+                origen = N;
+            } else
+            {
+                origen = I;
+            }
+
+            //Descripcion
+            descripcion = txtDescripcion.Text;
+
+            //Precio
+            precio = float.Parse(txtPrecio.Text);
+
+
+
+
+            //Guardo los datos en el arreglo
+
+            repuestos[posicion].numero = numRepuesto;
+            repuestos[posicion].marca = marca;
+            repuestos[posicion].origen = origen;
+            repuestos[posicion].descripcion = descripcion;
+            repuestos[posicion].precio = precio;
+            posicion++;
+
+
+            
+
+            
+            
+            //Limpiar interfaz
+            txtNumRepuesto.Text = "";
+            txtDescripcion.Text = "";
+            txtPrecio.Text = "";
+
+            
+           
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            // Evento donde se consulta los repuestos aplicando filtros por marca y por origen.
+
+            // Los filtros deben estar combinados,hay 3 casos
+
+            // 1.Repuestos solo por Marca
+            // 2.Respuestos solo por Origen
+            // 3.Repuestos por marca y origen
+
+
+            //Limpio primero la grilla sino se van acumulando una atras de otra
+
+            dataGrid.Rows.Clear();
+
+            //Se almacena la marca seleccionada en el filtro
+
+            marcaFiltro = cmbMarcaConsulta.SelectedItem.ToString();
+
+            //Se almacena el origen seleccionado en el filtro
+
+            if (optNacionalConsulta.Checked)
+            {
+
+                origenFiltro = N;
+            }
+            else
+            {
+
+                origenFiltro = I;
+            }
+
+
+
+            // --> Respuestos por marca y origen
+
+            // 1.Si tengo valores en marcaFiltro y origenFiltro es porque fueron seleccionados
+            // 2.Itero sobre repuestos 
+            // 3.Por cada elemento de repuestos pregunto si cumple con las dos condiciones
+            // 4.Muestro en la grilla los elementos que cumplen con las dos condiciones
+
+            if (marcaFiltro != null && origenFiltro != null )
+            {
+                for ( int i = 0; i < posicion; i++)
+                {
+                    if (repuestos[i].marca == marcaFiltro && repuestos[i].origen == origenFiltro)
+                    {
+
+                        dataGrid.Rows.Add(
+          repuestos[i].numero.ToString(),
+          repuestos[i].marca,
+          repuestos[i].origen,
+          repuestos[i].descripcion,
+          repuestos[i].precio.ToString()
+
+          );
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Evento donde se consultan todos los repuestos registrados
+
+            //Limpio primero la grilla
+
+            dataGrid.Rows.Clear();
+
+
+            //Itero sobre el arreglo de repuestos con la posicion como limite. 
+
+            for (int i = 0; i < posicion; i++)
+            {
+                dataGrid.Rows.Add(
+           repuestos[i].numero.ToString(),
+           repuestos[i].marca,
+           repuestos[i].origen,
+           repuestos[i].descripcion,
+           repuestos[i].precio.ToString()
+
+           );
+            }
+        }
+    }
+}
